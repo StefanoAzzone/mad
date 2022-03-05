d350633313d618f0f69817c3ad22ca34dbe3a665
Stefano Azzone <stefano.azzone@mail.polimi.it>
2022-03-05 17:58:33 +0100

    better PlayListList

--- lib/components/PlaylistList.dart
+++ lib/components/PlaylistList.dart
@@ -20,56 +20,67 @@ class _PlaylistListState extends State<PlaylistList> {
         return Scaffold(
             body: GridView.count(
               padding: EdgeInsets.only(top: 0.0),
-              childAspectRatio: 10,
+              childAspectRatio: 7,
               crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
               shrinkWrap: true,
               children: List.generate(playlists.length, (index) {
-                return GestureDetector(
-                    onTapDown: (TapDownDetails details) {
-                      _tapPosition = details.globalPosition;
-                    },
-                    onLongPress: () {
-                      RenderObject? overlay =
-                          Overlay.of(context)?.context.findRenderObject();
-                      Size? size = overlay?.semanticBounds.size;
-                      if (size != null) {
-                        showMenu(
-                            context: context,
-                            position: RelativeRect.fromRect(
-                                _tapPosition &
-                                    const Size(
-                                        40, 40), // smaller rect, the touch area
-                                Offset.zero &
-                                    size // Bigger rect, the entire screen
-                                ),
-                            items: <PopupMenuEntry<String>>[
-                              PopupMenuItem<String>(
-                                  value: 'Value1',
-                                  child: Column(children: [
-                                    TextButton(
-                                        onPressed: () async {
-                                          Navigator.pop(context);
-                                          database.playlists.removeAt(index);
-                                          setState(() {});
-                                        },
-                                        child: const Text(
-                                          "Delete playlist",
-                                          style: TextStyle(color: Colors.black),
-                                        )),
-                                  ]))
-                            ]);
-                      }
-                    },
-                    child: ListTile(
-                      title: Text(
-                        playlists[index].name,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      onTap: () {
-                        Navigator.pushNamed(context, '/showPlaylist',
-                            arguments: playlists[index]);
+                return Container(
+                  decoration: const BoxDecoration(
+                      border:
+                      //Border.all(color: Colors.black, width: 0.05)),
+                      Border(
+                        bottom: BorderSide(width: 0.5, color: Colors.grey),
+                        left: BorderSide(width: 0.5, color: Colors.grey),
+                      )),
+                  child: GestureDetector(
+                      onTapDown: (TapDownDetails details) {
+                        _tapPosition = details.globalPosition;
                       },
-                    ));
+                      onLongPress: () {
+                        RenderObject? overlay =
+                        Overlay.of(context)?.context.findRenderObject();
+                        Size? size = overlay?.semanticBounds.size;
+                        if (size != null) {
+                          showMenu(
+                              context: context,
+                              position: RelativeRect.fromRect(
+                                  _tapPosition &
+                                  const Size(
+                                      40, 40), // smaller rect, the touch area
+                                  Offset.zero &
+                                  size // Bigger rect, the entire screen
+                              ),
+                              items: <PopupMenuEntry<String>>[
+                                PopupMenuItem<String>(
+                                    value: 'Value1',
+                                    child: Column(children: [
+                                      TextButton(
+                                          onPressed: () async {
+                                            Navigator.pop(context);
+                                            database.playlists.removeAt(index);
+                                            setState(() {});
+                                          },
+                                          child: const Text(
+                                            "Delete playlist",
+                                            style: TextStyle(color: Colors.black),
+                                          )),
+                                    ]))
+                              ]);
+                        }
+                      },
+                      child: ListTile(
+                        title: Text(
+                          playlists[index].name,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        onTap: () {
+                          Navigator.pushNamed(context, '/showPlaylist',
+                              arguments: playlists[index]);
+                        },
+                      )
+                  )
+                );
+
               }),
             ),
             floatingActionButtonLocation:
